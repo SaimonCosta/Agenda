0d8ac568f54d47a2138a6fa751f33a53985579c8
SaimonCosta <saimonsantoscosta@outlook.com>
2024-06-14 12:47:34 -0300

    Inicialização da validação de dados + ajustes no HTML ( Textos)

--- JS/funcoes.js
+++ JS/funcoes.js
@@ -1,3 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    Inputmask("(99) 99999-9999").mask(document.getElementById("telefone"));
-});
+const nome = document.querySelector('#usuario');
+const senha = document.querySelector('#senha');
+const email = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
